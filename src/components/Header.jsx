9f18bc426fd38f3da6bac9ee21d2c071f8d0112b
Simon Graham ☘️ <70765754+s-n-graham@users.noreply.github.com>
2023-06-14 17:01:28 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,9 +10,9 @@ function Header() {
          <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold text-zinc-700 tracking-tighter">Simon Graham</h1>
          <p className="text-base md:text-xl mb-3 font-medium text-zinc-800">Developer / Designer</p>
          <p className="text-sm max-w-xl mb-6 text-zinc-500 font-sans">
-            Haigh, hello ja hei! I'm a multifaceted and humble guy from Ireland&#9752;and the UK&#127984;! I've been finding my way in Finland for the last 6 years and counting, it's been quite the journey!
-            I'm currently exploring frontend development with a growing appreciation for the cloud, integrations and backend sorcery. I know this is what I'd love to learn and do everyday; in this
-            I can succeed.  
+            Haigh, hello ja hei! I'm a multifaceted and humble guy from Ireland&#9752;and the UK&#127984;! I've been finding my way in Finland for the last 8 years and counting
+            where after graduation, gained solid working knowledge of software development and design, multicloud, some data analysis visualisation and integration backend sorcery.
+            My lifelong talents and passion shine brightest with design, strategy and people.  
             <br />
             I could have stayed an entrepreneur in music and media but have chosen to enjoy that as a sidequest. I'm currently building a React, Redux TypeScript gym app to stay in shape but here are a few other things I've done.
          </p>
